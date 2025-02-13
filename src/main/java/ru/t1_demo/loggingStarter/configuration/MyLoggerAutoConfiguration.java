07736ceab8b50abe75c15e88c842212624fe19d8
arvoglade <arvoglade@gmail.com>
2025-02-14 03:46:34 +0500

    starter bean creation

--- src/main/java/ru/t1_demo/loggingStarter/configuration/MyLoggerAutoConfiguration.java
+++ src/main/java/ru/t1_demo/loggingStarter/configuration/MyLoggerAutoConfiguration.java
@@ -1,9 +1,16 @@
 package ru.t1_demo.loggingStarter.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.t1_demo.loggingStarter.myLogger.MyLogger;
 
 @Configuration
 public class MyLoggerAutoConfiguration {
 
+    @Bean
+    public MyLogger myLogger() {
+        String text = "default text";
+        return new MyLogger(text);
+    }
 
 }
---
+++ src/main/java/ru/t1_demo/loggingStarter/myLogger/MyLogger.java
@@ -0,0 +1,10 @@
+package ru.t1_demo.loggingStarter.myLogger;
+
+public class MyLogger {
+
+    public String text;
+
+    public MyLogger(String text) {
+        this.text = text;
+    }
+}
