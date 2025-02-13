764886e8593800285517173fc728a89cf5ca8c8d
arvoglade <arvoglade@gmail.com>
2025-02-14 03:40:01 +0500

    initial commit

---
+++ src/main/java/ru/t1_demo/loggingStarter/MyLoggingSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package ru.t1_demo.loggingStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLoggingSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLoggingSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/t1_demo/my_logging_spring_boot_starter/MyLoggingSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.t1_demo.my_logging_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLoggingSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
